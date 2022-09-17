9d30dbd780f7348a4c66bbc21c55d804c544dc42
Colleen Cabedo <cmcabedo@gmail.com>
2022-09-16 20:38:39 -0400

    commit tree 35cabcb5558995ccd959601f5df0e0c5aff35bb6

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = (num1 * num2);
+console.log(multiply);
+
+
+let random = Math.floor(Math.random() * 1) + 1;;
+
+
+let num3 = 12;
+let num4 = 8;
+
+let mod = (num3 % num4);
+console.log(mod);
+
+let max = Math.max(5, 8, 20, 6);
+console(max);
